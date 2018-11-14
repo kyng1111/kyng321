f466b0f46a11b381ee241925b0d646cd5fb1b0d3
kyng1111 <41780361+kyng1111@users.noreply.github.com>
2018-11-15 01:59:47 +0900

    Update resnet.py

--- train/resnet.py
+++ train/resnet.py
@@ -27,7 +27,7 @@ def unpickle(file):
 
 
 for i in range(1,6):
-    a = unpickle("\data_batch_"+str(i))
+    a = unpickle("data_batch_"+str(i))
     r = a['data'][:,0:1024].reshape((-1,1))
     g = a['data'][:,1024:2048].reshape((-1,1))
     b = a['data'][:,2048:3072].reshape((-1,1))
