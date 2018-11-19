4cb62e886a082ca6fdb2423f3df0d44435a2bd80
kyng1111 <41780361+kyng1111@users.noreply.github.com>
2018-11-19 14:53:18 +0900

    Update resnet.py

--- train/resnet.py
+++ train/resnet.py
@@ -17,6 +17,29 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('input_dir', 'input', 'Input Directory.')
 flags.DEFINE_string('output_dir', 'output', 'Output Directory.')
 
+
+EPOCHS = 10
+batch_size = 1000
+n_batch = 50
+image = np.zeros((1,3072))
+label = np.zeros((1,1))
+tf.reset_default_graph()
+
+for i in range(1,6):
+    path = os.path.join(FLAGS.input_dir,"data_batch_"+str(i))
+    a = unpickle(path)
+    r = a['data'][:,0:1024].reshape((-1,1))
+    g = a['data'][:,1024:2048].reshape((-1,1))
+    b = a['data'][:,2048:3072].reshape((-1,1))
+    l = np.array(a['labels']).reshape((-1,1))
+    image1 = np.hstack([r,g,b]).reshape((-1,3072))
+    label1 = l
+    image = np.vstack((image1,image))
+    label = np.vstack((label1,label))
+
+
+
+
 def unpickle(file):
     import pickle
     with file_io.FileIO(file,'rb') as fo:
@@ -115,29 +138,9 @@ a = resnet()
 
 
 def main(_):
-    
-    EPOCHS = 10
-    batch_size = 1000
-    n_batch = 50
-    image = np.zeros((1,3072))
-    label = np.zeros((1,1))
-    tf.reset_default_graph()
-    
-    for i in range(1,6):
-        path = os.path.join(FLAGS.input_dir,"data_batch_"+str(i))
-        a = unpickle(path)
-        r = a['data'][:,0:1024].reshape((-1,1))
-        g = a['data'][:,1024:2048].reshape((-1,1))
-        b = a['data'][:,2048:3072].reshape((-1,1))
-        l = np.array(a['labels']).reshape((-1,1))
-        image1 = np.hstack([r,g,b]).reshape((-1,3072))
-        label1 = l
-        image = np.vstack((image1,image))
-        label = np.vstack((label1,label))
 
     print(image.shape)
     print(label.shape)
-
     
     k = resnet()
 
