6f935b752113b02cfee87d2f823d6447b8b11775
kyng1111 <41780361+kyng1111@users.noreply.github.com>
2019-01-04 01:48:47 +0900

    Update RNN.py

--- train/RNN.py
+++ train/RNN.py
@@ -36,7 +36,7 @@ learning_rate = 0.01
 path = os.path.join(FLAGS.input_dir,"final_input.txt")
 
 
-a = unpickle("final_input.txt")
+a = unpickle(path)
 b1 = np.array(a).reshape(-1,200)
 
 print(b1.shape)
